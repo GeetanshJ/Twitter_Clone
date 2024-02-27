70972e82561113d46364d9c20ed252ff2a04d1fa
GeetanshJ <geetansh0513.be21@chitkara.edu.in>
2024-02-27 12:41:29 +0530

    search and multer done

--- server.js
+++ server.js
@@ -3,51 +3,60 @@ const app = express();
 const session = require("express-session");
 const bodyParser = require("body-parser");
 const db = require("./database");
-var multer = require('multer');
-app.use(session({ secret: "g#a%t&v%i#t%" }));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
+const multer = require('multer');
 const verify_Mail = require('./nodemailer');
-app.use(express.static(__dirname+"/public"))
-var multer = require('multer');
 
-var storage = multer.diskStorage({
+// Session configuration
+app.use(session({ secret: "g#a%t&v%i#t%" }));
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Set view engine to EJS
+app.set("view engine", "ejs");
+
+// Serve static files from the 'public' directory
+app.use(express.static(__dirname + "/public"));
+
+// Multer configuration for file uploads
+const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/')
+    cb(null, './public'); // Upload files to the 'uploads' directory
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() +"--"+file.originalname)
+    cb(null, "./uploads/"+file.originalname); // Use the original filename for uploaded files
   }
-})
+});
+const upload = multer({ storage: storage });
+
+// Routes
+
+// Homepage
 app.get("/", (req, res) => {
-  let msg = "";
-  if (req.session.msg != "") {
-    msg = req.session.msg;
-  }
+  let msg = req.session.msg || "";
   res.render("login", { msg: msg });
 });
 
-
-
-
-
+// Search route
 app.get("/search", (req, res) => {
   const search_str = req.query["search"];
-  console.log("Search string:", search_str);
-  let sql = "SELECT username FROM user WHERE uid IN (SELECT uid FROM tweet WHERE content LIKE '%" + search_str + "%' OR username LIKE '%" + search_str + "%')";
-  db.query(sql, [search_str], (err, rest, fields) => {
+  const sql = "SELECT * FROM user WHERE username LIKE ?";
+  db.query(sql, [`%${search_str}%`], (err, rest) => {
     if (err) {
-      console.error("Error executing search query:", err); // Log any errors
-      res.status(500).send("Internal Server Error"); // Respond with an error status
+      console.error(err);
+      res.status(500).send("Internal Server Error");
     } else {
-      console.log("Search results:", rest); // Log the search results
-      res.render("home", { result: rest, search: true }); // Render the home view with the search results
+
+      let sql1 = "SELECT tweet.*, user.* FROM tweet INNER JOIN user ON tweet.uid = user.uid WHERE tweet.uid = ? OR tweet.content LIKE CONCAT('%', ?, '%') OR tweet.uid IN (SELECT follow_uid FROM followers WHERE follow_uid = ?) ORDER BY tweet.datetime DESC;";
+      db.query(sql1, [req.session.uid, req.session.un, req.session.uid], (error, result, fields) => {
+
+        res.render("home", { result: rest, search: true, mssg: "", result_tweets: result });
+      });
     }
-  });
+  }); 
 });
 
-
-
+// Login route
 app.post("/login", (req, res) => {
   const { email, password } = req.body;
   let sql = "";
@@ -81,143 +90,113 @@ app.post("/login", (req, res) => {
   });
 });
 
+// Signup route
 app.get("/signup", (req, res) => {
   res.render("signup", { msg: "" });
 });
 
-
 app.post("/signup_success", (req, res) => {
-  const { username, fname, mname, lname, email, password, cpass, dob, gender } =
-    req.body;
-  let sql_check = "";
-  if (isNaN(email)) {
-    sql_check = `select email from user where email = '${email}'`;
-  } else {
-    sql_check = `select mobile from user where mobile = ${email}`;
-  }
-  db.query(sql_check, (err, result, fields) => {
-    if (err) throw err;
-    if (result.length == 1) {
-      let error_messg = "";
-      if (isNaN(email)) {
-        error_messg = "Email id already in use!";
+  const { username, fname, mname, lname, email, password, cpass, dob, gender } = req.body;
+  let sql_check = isNaN(email) ? `SELECT email FROM user WHERE email = ?` : `SELECT mobile FROM user WHERE mobile = ?`;
+  
+  db.query(sql_check, [email], (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Internal Server Error");
+    } else {
+      if (result.length === 1) {
+        let error_messg = isNaN(email) ? "Email id already in use!" : "Mobile Number already in use!";
+        res.render("signup", { msg: error_messg });
       } else {
-        error_messg = "Mobile Number already in use!";
-      }
-      res.render("signup", { msg: error_messg });
-    } // end of result.length function which checks whether email/mobile already in use or not
-    else {
-      // now we will register here by entering the user details
-      let sql = "";
-      if (isNaN(email)) {
-        sql =
-          "insert into user (username,fname,mname,lname,email,password,dob,dor,gender) values (?,?,?,?,?,?,?,?,?)";
-      } else {
-        sql =
-          "insert into user (username,fname,mname,lname,mobile,password,dob,dor,gender) values (?,?,?,?,?,?,?,?,?)";
-      }
-      let d = new Date();
-      let m = d.getMonth() + 1;
-      let dor = d.getFullYear() + "-" + m + "-" + d.getDate();
-      db.query(
-        sql,
-        [username, fname, mname, lname, email, password, dob, dor, gender],
-        (err, result, fields) => {
-          if (err) throw err;
-          if (result.insertId > 0) {
-            verify_Mail(email)
-            req.session.msg =
-              "Account created... Check email for verification link";
-            res.redirect("/");
+        let sql = isNaN(email) ?
+          "INSERT INTO user (username,fname,mname,lname,email,password,dob,dor,gender) VALUES (?,?,?,?,?,?,?,?,?)" :
+          "INSERT INTO user (username,fname,mname,lname,mobile,password,dob,dor,gender) VALUES (?,?,?,?,?,?,?,?,?)";
+
+        let d = new Date();
+        let m = d.getMonth() + 1;
+        let dor = d.getFullYear() + "-" + m + "-" + d.getDate();
+        db.query(sql, [username, fname, mname, lname, email, password, dob, dor, gender], (err, result) => {
+          if (err) {
+            console.error(err);
+            res.status(500).send("Internal Server Error");
           } else {
-            req.session.msg = "Registration Unsuccessful! Please try again";
+            if (result.insertId > 0) {
+              verify_Mail(email);
+              req.session.msg = "Account created... Check email for verification link";
+              res.redirect("/");
+            } else {
+              req.session.msg = "Registration Unsuccessful! Please try again";
+              res.redirect("/signup");
+            }
           }
-        }
-      );
+        });
+      }
     }
   });
 });
 
+// Logout route
 app.get("/logout", (req, res) => {
-  req.session.uid = "";
+  req.session.destroy();
   res.redirect("/");
 });
 
+// Tweet submit route
+app.post("/tweet-submit", upload.single("tweet_img"), (req, res) => {
+  const { tweet } = req.body;
+  const { filename, mimetype } = req.file;
 
-
-
-const storage_config = multer({storage:storage})
-app.post('/tweet-submit', storage_config.single("tweet_img"),(req, res) => {
-  const tweet = req.body.tweet;
-  var filename = "";
-  var mimetype = "";
-
-  try{
-    filename=req.file.filename;
-    mimetype=req.file.mimetype;
-  }
-
-  catch(err){
-    console.log(err);
-  }
-
-  let sql = "insert into tweet (uid,content,datetime,imagevdo_name,type) values(?,?,?,?,?)"
+  let sql = "INSERT INTO tweet (uid,content,datetime,imagevdo_name,type) VALUES(?,?,?,?,?)";
   let date = new Date();
   let month = date.getMonth() + 1;
-  let date_time = date.getFullYear() + "-" + month + "-" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-  db.query(sql, [req.session.uid, tweet, date_time,filename,mimetype], (err, result) => {
+  let date_time = `${date.getFullYear()}-${month}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  db.query(sql, [req.session.uid, tweet, date_time, filename, mimetype], (err, result) => {
     if (err) {
-      console.log(err);
-    }
-
-    else {
+      console.error(err);
+      res.status(500).send("Internal Server Error");
+    } else {
       if (result.insertId > 0) {
         res.redirect("/home");
-      }
-      
-      else {
+      } else {
         res.render('home', { mssg: "Unable to post tweet!" });
       }
     }
   });
-})
+});
 
+// Homepage route
 app.get("/home", (req, res) => {
-  if (req.session.uid != "") {
-    let msg = "";
-    msg = req.session.msg;
-
-    let sql = "SELECT tweet.*, user.username FROM tweet INNER JOIN user ON tweet.uid = user.uid WHERE tweet.uid = ? OR user.uid = ? ORDER BY tweet.datetime DESC";
-    db.query(sql, [req.session.uid, req.session.uid], (err, result, fields) => {
-      if(err) throw err;
-      else{
-        const search_str = req.query["search"];
-        let sql = "SELECT username FROM user WHERE uid IN (SELECT uid FROM tweet WHERE content LIKE '%" + search_str + "%' OR username LIKE '%" + search_str + "%')";
-        db.query(sql, [search_str], (err, rest, fields) => {
-          res.render("home", {result: rest,search: true,mssg: "Tweet Posted", result_tweets: result});
-        });
+  if (req.session.uid) {
+    let msg = req.session.msg || "";
+    const sql = "SELECT tweet.*, user.username FROM tweet INNER JOIN user ON tweet.uid = user.uid WHERE tweet.uid = ? OR tweet.content LIKE CONCAT('%', ?, '%') OR tweet.uid IN (SELECT follow_uid FROM followers WHERE follow_uid = ?) ORDER BY tweet.datetime DESC";
+    db.query(sql, [req.session.uid, req.session.un, req.session.uid], (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send("Internal Server Error");
+      } else {
+        res.render("home", { result_tweets: result, result: [], search: false, mssg: msg });
       }
-    }
-    );
-  }
-
-  else {
-    req.session.msg = "Please login first to view home page!";
+    });
+  } else {
     res.redirect("/");
   }
 });
 
+// Following route
 app.get("/following", (req, res) => {
-  let sql =
-    "select * from user where uid in (select follow_uid from followers where uid = ?)"; // sql to print to whom am I following
+  let sql = "SELECT * FROM user WHERE uid IN (SELECT follow_uid FROM followers WHERE uid = ?)";
   db.query(sql, [req.session.uid], (err, result) => {
-    if (err) throw err;
-    res.render("following_list", { following_result: result });
+    if (err) {
+      console.error(err);
+      res.status(500).send("Internal Server Error");
+    } else {
+      res.render("following_list", { following_result: result });
+    }
   });
 });
 
-
-
-
-
-app.listen(9000, () => console.log("Server running at http://localhost:9000"));
+// Server listening 
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
